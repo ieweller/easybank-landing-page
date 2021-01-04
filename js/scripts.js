61381d1c22f5cd1858efe7c44a72feeae116385b
Ivan Weller <ivan@wellerindependent.com>
2021-01-04 12:21:59 -0800

    converted latest article previews to .json file, started basic display of latest articles area

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,39 @@
+// toggle mobile menu when hamburger/close icon is touched
 $(".menu").click(function(){
   $("nav").fadeToggle(50);
   $(".overlay").fadeToggle(200);
   $(".menu").toggleClass('close');
   $("body").toggleClass('hide-overflow');
 });
+
+$(function() {
+  $.ajax({
+    type : 'GET',
+    url : 'articles.json',
+    async : false,
+    beforeSend : function(){/*loading*/},
+    dataType : 'json',
+    success : function(result) {
+      latestArticles(result);
+    }
+  });
+});
+
+function latestArticles(articlesList) {
+  var buffer = "";
+
+  $.each(articlesList, function(index, val){
+
+    buffer += '<article><img src="';
+    buffer += val.thumbnail;
+    buffer += '"><span rel="author">By ';
+    buffer += val.author;
+    buffer += '</span><h1>';
+    buffer += val.title;
+    buffer += '</h1><p>';
+    buffer += val.content;
+    buffer += '</p></article>';
+  });
+  // Adds table generated from JSON objects to DOM
+  $('.latest-articles').html(buffer);
+}
