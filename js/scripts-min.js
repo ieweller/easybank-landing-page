822eb0726bcc9b700b1b5265ca6f09854293bad2
Ivan Weller <ivan@wellerindependent.com>
2021-01-04 11:22:24 -0800

    updated overflow rules

--- js/scripts-min.js
+++ js/scripts-min.js
@@ -1,1 +1,1 @@
-$(".menu").click((function(){$("nav").fadeToggle(50),$(".overlay").fadeToggle(200),$(".menu").toggleClass("close")}));
+$(".menu").click((function(){$("nav").fadeToggle(50),$(".overlay").fadeToggle(200),$(".menu").toggleClass("close"),$("body").toggleClass("hide-overflow")}));
--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,5 @@ $(".menu").click(function(){
   $("nav").fadeToggle(50);
   $(".overlay").fadeToggle(200);
   $(".menu").toggleClass('close');
+  $("body").toggleClass('hide-overflow');
 });
